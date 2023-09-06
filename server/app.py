cf690d781712817f6a46de0ee27f3b3771ced88e
Leroy G <liguan698@gmail.com>
2023-09-06 14:43:56 -0700

    complete lab

--- server/app.py
+++ server/app.py
@@ -6,33 +6,63 @@ from flask_migrate import Migrate
 from models import db, Bakery, BakedGood
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return '<h1>Bakery GET API</h1>'
+    return "<h1>Bakery GET API</h1>"
 
-@app.route('/bakeries')
+
+@app.route("/bakeries")
 def bakeries():
-    return ''
+    bakery_query = Bakery.query.all()
+    bakeries_list = [bakery.to_dict() for bakery in bakery_query]
 
-@app.route('/bakeries/<int:id>')
+    response = make_response(jsonify(bakeries_list), 200)
+
+    response.headers["Content-Type"] = "application/json"
+    return response
+
+
+@app.route("/bakeries/<int:id>")
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+    bakery_serialized = bakery.to_dict()
+    response = make_response(jsonify(bakery_serialized), 200)
 
-@app.route('/baked_goods/by_price')
+    response.headers["Content-Type"] = "application/json"
+    return response
+
+
+@app.route("/baked_goods/by_price")
 def baked_goods_by_price():
-    return ''
+    baked_price = BakedGood.query.order_by(BakedGood.price).all()
+    baked_goods_by_price_serialized = [price.to_dict() for price in baked_price]
+    response = make_response(jsonify(baked_goods_by_price_serialized), 200)
 
-@app.route('/baked_goods/most_expensive')
+    response.headers["Content-Type"] = "application/json"
+    return response
+
+
+@app.route("/baked_goods/most_expensive")
 def most_expensive_baked_good():
-    return ''
+    most_expensive_good = (
+        BakedGood.query.order_by(BakedGood.price.desc()).limit(1).first()
+    )
+    most_expensive_good_serialized = most_expensive_good.to_dict()
 
-if __name__ == '__main__':
+    response = make_response(jsonify(most_expensive_good_serialized), 200)
+
+    response.headers["Content-Type"] = "application/json"
+    return response
+
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
